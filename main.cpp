66a4844f702a6b25105725d2b75c554ac8a0c9dd
cri-cov <cristian.covato@marconibari.edu.it>
2025-04-09 22:13:56 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,80 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    
+    bool scambio;
+    int n, i, f, g, j, t, s;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "quanti numeri generare?" << endl;
+    cin >> n;
+    int p[n], d[n], v[n];
+
+    i = 0;
+    while (i < n) {
+        v[i] = rand() % n;
+        i = i + 1;
+    }
+    i = 0;
+    f = 0;
+    g = 0;
+    while (i < n) {
+        if (v[i] % 2 == 0) {
+            p[f] = v[i];
+            f = f + 1;
+        } else {
+            d[g] = v[i];
+            g = g + 1;
+        }
+        i = i + 1;
+    }
+    scambio = true;
+    i = 0;
+    while (i <= g - 1 && scambio) {
+        scambio = false;
+        j = 0;
+        while (j <= g - 2 - i) {
+            if (d[j] > d[j + 1]) {
+                t = d[j];
+                d[j] = d[j + 1];
+                d[j + 1] = t;
+                scambio = true;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    scambio = true;
+    i = 0;
+    while (i <= f - 1 && scambio) {
+        scambio = false;
+        j = 0;
+        while (j <= f - 2 - i) {
+            if (p[j] > p[j + 1]) {
+                t = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = t;
+                scambio = true;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    f = 0;
+    g = 0;
+    while (i < f) {
+        cout << p[i] << endl;
+        i = i + 1;
+    }
+    i = 0;
+    while (i < g) {
+        cout << d[i] << endl;
+        i = i + 1;
+    }
+    i = g;
+    while (i > 0) {
+        cout << d[i] << endl;
+        i = i - 1;
+    }
+    return 0;
+}
